f12f6fa00342c34bd678a14cf1936a7a02bc23d7
Edem Dumenu <ehdemdume@gmail.com>
2020-09-14 23:35:14 -0400

    Responsive chart
    Added responsiveness to the bar charts

--- resources/js/src/admin/components/CardSummary.js
+++ resources/js/src/admin/components/CardSummary.js
@@ -10,11 +10,11 @@ function CardSummary() {
             <div className="row mt-4">
                 <div className="col-lg-3 col-md-6">
                     <div className="card mb-3 shadow text-blue">
-                        <div class="d-flex card-header">
+                        <div className="d-flex card-header">
                             <div>Number of Reports</div>
                             <div className="ml-auto">
-                                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-newspaper" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5v-11zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5H12z" />
+                                <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-newspaper" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5v-11zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5H12z" />
                                     <path d="M2 3h10v2H2V3zm0 3h4v3H2V6zm0 4h4v1H2v-1zm0 2h4v1H2v-1zm5-6h2v1H7V6zm3 0h2v1h-2V6zM7 8h2v1H7V8zm3 0h2v1h-2V8zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1z" />
                                 </svg>
                             </div>
@@ -26,12 +26,12 @@ function CardSummary() {
                 </div>
                 <div className="col-lg-3 col-md-6">
                     <div className="card mb-3 shadow text-blue">
-                        <div class="d-flex card-header">
+                        <div className="d-flex card-header">
                             <div>Number of studies</div>
                             <div className="ml-auto">
-                                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-folder" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-folder" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M9.828 4a3 3 0 0 1-2.12-.879l-.83-.828A1 1 0 0 0 6.173 2H2.5a1 1 0 0 0-1 .981L1.546 4h-1L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3v1z" />
-                                    <path fill-rule="evenodd" d="M13.81 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zM2.19 3A2 2 0 0 0 .198 5.181l.637 7A2 2 0 0 0 2.826 14h10.348a2 2 0 0 0 1.991-1.819l.637-7A2 2 0 0 0 13.81 3H2.19z" />
+                                    <path fillRule="evenodd" d="M13.81 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zM2.19 3A2 2 0 0 0 .198 5.181l.637 7A2 2 0 0 0 2.826 14h10.348a2 2 0 0 0 1.991-1.819l.637-7A2 2 0 0 0 13.81 3H2.19z" />
                                 </svg>
                             </div>
                         </div>
@@ -42,13 +42,13 @@ function CardSummary() {
                 </div>
                 <div className="col-lg-3 col-md-6">
                     <div className="card mb-3 shadow text-blue">
-                        <div class="d-flex card-header">
+                        <div className="d-flex card-header">
                             <div>Number of programmers</div>
                             <div className="ml-auto">
-                                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-person-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-person-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z" />
-                                    <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                                    <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z" />
+                                    <path fillRule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                    <path fillRule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z" />
                                 </svg>
                             </div>
                         </div>
@@ -60,11 +60,11 @@ function CardSummary() {
                 <div className="col-lg-3 col-md-6">
 
                     <div className="card mb-3 shadow text-blue">
-                        <div class="d-flex card-header">
+                        <div className="d-flex card-header">
                             <div>Number of Data Managers</div>
                             <div className="ml-auto">
-                                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-person-badge-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z" />
+                                <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-person-badge-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z" />
                                 </svg>
                             </div>
                         </div>
--- resources/js/src/admin/components/ReportChart.js
+++ resources/js/src/admin/components/ReportChart.js
@@ -1,39 +1,58 @@
 import React from 'react';
-import {
-    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-} from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
+import { curveCardinal } from 'd3-shape';
 
 function ReportChart() {
 
-    // const { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } = Recharts;
     const data = [
-        { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-        { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-        { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-        { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-        { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-        { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-        { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+        {
+            name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+        },
+        {
+            name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+        },
+        {
+            name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+        },
+        {
+            name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+        },
+        {
+            name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+        },
+        {
+            name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+        },
+        {
+            name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+        },
     ];
 
+    const cardinal = curveCardinal.tension(0.2);
+
     return (
-        <>
-            <div className="card shadow-sm mb-5">
-                <div className="card-header font-weight-bold">Bar Chart</div>
-                {/* <h4 className="mt-3">Bar Chart</h4> */}
-                <h6 className="text-center mt-2">A bar chart provides a way of showing data values represented as vertical bars.</h6>
-                <BarChart width={500} height={250} data={data}
-                    margin={{ top: 20, right: 10, left: 10, bottom: 5 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="pv" fill="#8884d8" />
-                    <Bar dataKey="uv" fill="#e5e7ee" />
-                </BarChart>
+        <div className="card shadow-sm mb-5">
+            <div className="card-header font-weight-bold">Bar Chart</div>
+            {/* <h4 className="mt-3">Bar Chart</h4> */}
+            <h6 className="text-center mt-2">A bar chart provides a way of showing data values represented as vertical bars.</h6>
+            <div style={{ width: '100%', height: 300 }}>
+                <ResponsiveContainer>
+                    <AreaChart
+                        data={data}
+                        margin={{
+                            top: 10, right: 30, left: 0, bottom: 0,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+                        <Area type={cardinal} dataKey="uv" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.3} />
+                    </AreaChart>
+                </ResponsiveContainer>
             </div>
-        </>
+        </div>
     );
 }
 
