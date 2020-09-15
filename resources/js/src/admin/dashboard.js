a2936d12ab483d2c9c5613c81350db24470e2253
Edem Dumenu <ehdemdume@gmail.com>
2020-09-15 09:25:14 -0400

    Calendar
    Added a calendar to the admin page

--- resources/js/src/admin/dashboard.js
+++ resources/js/src/admin/dashboard.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import CardSummary from './components/CardSummary';
 import ReportChart from './components/ReportChart';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export class dashboard extends Component {
     render() {
         return (
             <div className="container-fluid dashboardCard text-blue">
                 <CardSummary />
-                <div className="row">
+                <div className="row mt-1">
                     <div className="col-12 col-md-6"> <ReportChart /></div>
-                    <div className="col-12 col-md-6"> <ReportChart /> </div>
+                    <div className="col-12 col-md-6"><Calendar className="calendarStyle shadow" /></div>
                 </div>
                 {/* <ReportChart /> */}
             </div>
