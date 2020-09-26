bd85074013b7ec06ffe766138ecf916992632d01
Edem Dumenu <ehdemdume@gmail.com>
2020-09-26 08:02:47 -0400

    Pagination
    Clean up pagination page

--- resources/js/src/components/Pagination.js
+++ resources/js/src/components/Pagination.js
@@ -7,9 +7,6 @@ export default function Pagination({ comments, searchComments }) {
                 <li className={`page-item ${comments.prev_page_url === null ? "disabled" : ""}`}>
                     <button className="page-link" onClick={() => searchComments("previous", null)} tabIndex="-1">Previous</button>
                 </li>
-                <li className={`page-item ${comments.current_page - 1 === 0 ? "d-none" : ""}`}><a className="page-link" onClick={() => searchComments("pageSelect", (comments.current_page) - 1)}>{(comments.current_page) - 1}</a></li>
-                <li className={`page-item ${comments.current_page === 0 ? "d-none" : ""}`}><a className="page-link" onClick={() => searchComments("pageSelect", comments.current_page)}>{comments.current_page}</a></li>
-                <li className={`page-item ${(comments.current_page + 1) === comments.last_page || comments.total == 1 ? "d-none" : ""}`}><a className="page-link" onClick={() => searchComments("pageSelect", (comments.current_page) + 1)}>{(comments.current_page) + 1}</a></li>
                 <li className={`page-item ${comments.next_page_url === null ? "disabled" : ""}`}>
                     <button className="page-link" onClick={() => searchComments("next", null)}>Next</button>
                 </li>
